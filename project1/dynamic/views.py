62b3ee29e0e2741545fe1b8ac8f3e15e19ac0898
ItsMeSwaron <swaron3214@gmail.com>
2025-03-09 19:53:44 +0600

    2nd Commit

--- project1/dynamic/views.py
+++ project1/dynamic/views.py
@@ -2,5 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 
+d = {
+    'contents':[
+        {
+            'name':'azmain',
+            'id':223089,
+            'cgpa':4.00,    
+        },
+        {
+            'name':'amirul',
+            'id':2230419,
+            'cgpa':3.50,
+        },
+        {
+            'name':'dipto',
+            'id':2230470,
+            'cgpa':3.26,
+        },
+    ],
+}
+
 def printer(request):
-    return render(request, 'dynamic_view.html')
+    return render(request, 'dynamic_view.html', d)
